3b072ce7316fa3faba6ba1660b812ccba1f8df2e
gcz <798862903@qq.com>
2018-04-21 08:10:29 +0800

    修正数据生成器

--- data_loader.py
+++ data_loader.py
@@ -7,33 +7,46 @@ import cv2
 import keras
 
 class DataIter(keras.utils.Sequence):
-    def __init__(self, data_list, image_shape, img_channel, classes, batch_size,shuffle=False):
-        self.dataset_list= []
+    def __init__(self, data_list, image_shape, image_channel, classes, batch_size,shuffle=False):
+        self.image_list= []
+        self.label_list= []
         for m_line in open(data_list):
-            self.dataset_list.append(m_line.strip('\n'))
+            image_path,label = m_line.strip('\n').split(' ')
+            self.image_list.append(image_path)
+            self.label_list.append(label)
+        self.image_list = np.array(self.image_list)
+        self.label_list = np.array(self.label_list)
         self.batch_size = batch_size
         self.image_shape = image_shape
-        self.img_channel = img_channel
+        self.image_channel = image_channel
         self.classes = classes
         self.shuffle = shuffle
 
     def __len__(self):
-        return int(np.floor(len(self.dataset_list) / self.batch_size))
+        return int(np.floor(len(self.image_list) / self.batch_size))
 
     def load_img(self,img_path):
-        img = cv2.imread(img_path)
+        if self.image_channel == 3:
+            img = cv2.imread(img_path)
+        else:
+            img = cv2.imread(img_path,0)
         img = cv2.resize(img, (self.image_shape[0], self.image_shape[1]))
+        img = np.reshape(img,[self.image_shape[0], self.image_shape[1],self.image_channel])
         return img
 
     def __getitem__(self, idx):
-        batch_list = self.dataset_list[idx * self.batch_size: (idx + 1) * self.batch_size]
-        batch_x = []
-        batch_y = []
-        for i, line in enumerate(batch_list):
-            img_path, label = line.split(' ')
-            img = self.load_img(img_path)
-            m_label = np.zeros(len(self.classes))
-            m_label[self.classes.index(label)] = 1
-            batch_x.append(img)
-            batch_y.append(m_label)
-        return np.array(batch_x),np.array(batch_y)
+        batch_x = self.image_list[idx * self.batch_size: (idx + 1) * self.batch_size]
+        batch_y = self.label_list[idx * self.batch_size: (idx + 1) * self.batch_size]
+        return np.array([self.load_img(file_name) for file_name in batch_x]), np.array(batch_y)
+        #     img_path, label = line.split(' ')
+        #     img = self.load_img(img_path)
+        #     m_label = np.zeros(len(self.classes))
+        #     m_label[self.classes.index(label)] = 1
+        #     batch_x.append(img)
+        #     batch_y.append(m_label)
+        # return np.array(batch_x),np.array(batch_y)
+
+    def on_epoch_end(self):
+        index = np.random.shuffle(list(len(self.image_list)))
+        self.image_list = self.image_list[index]
+        self.label_list = self.label_list[index]
--- train.py
+++ train.py
@@ -26,9 +26,9 @@ train_datagen = keras.preprocessing.image.ImageDataGenerator()
 
 # 使用自己的数据生成器
 classes = [str(i) for i in range(10)]
-train_generator = DataIter(data_list=path + '/' + label_file, image_shape=[227, 227], img_channel=3,
+train_generator = DataIter(data_list=path + '/' + label_file, image_shape=[227, 227], image_channel=3,
                            classes=classes,batch_size=batch_size)
 model = AlexNet(num_classes=10)
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-model.fit_generator(train_generator, steps_per_epoch=782, epochs=1,verbose=1)
+model.fit_generator(train_generator, steps_per_epoch=1, epochs=1,verbose=1)
 model.save('alexnet.h5')
